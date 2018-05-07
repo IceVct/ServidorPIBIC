9b9eee67f359154ffcb47c552987d40e8b1e1e1b
icevct <icevct@gmail.com>
2018-05-07 17:06:31 -0300

    quase pronto

--- routes/imagem.js
+++ routes/imagem.js
@@ -8,21 +8,23 @@ router.post('/imagem', (req, res) => {
   let destPath = './imagem/';
   let filename = 'foto';
   base64Img.img(req.body.foto, destPath, filename, function(err, filepath) {
-    return res.send({
-      filepath: filepath,
-      success: true
-    })
-  });
-  // const exec = require('child_process').exec;
-  // const child = exec('/home/victor/UnB/PIBIC_Bruno/TutorialOpenCV/test ./imagem/foto', function(error, stdout, stderr, callback) {
-  //   console.log(stdout);
-  //   return res.send({
-  //     resposta: stdout,
-  //     success: true
-  //   })
-  // });
+    // return res.send({
+    //  filepath: filepath,
+    //  success: true
+    // })	
+   let file = '/home/victor/UnB/PIBIC_Bruno/ServidorPIBIC/' + filepath;
+   console.log(file);
+   let comando = '/home/victor/UnB/PIBIC_Bruno/TutorialOpenCV/test ' + file;
+   const exec = require('child_process').exec;
+   const child = exec(comando, function(error, stdout, stderr, callback) {
+   	console.log(stdout);
+     return res.send({
+       resposta: stdout,
+       success: true
+     })
+   });
 
-  
+  });
 })
     
 module.exports = router;
